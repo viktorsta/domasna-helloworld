016a254b2886bd93e817de2ccbac33b993b154f2
Viktor S <80289514+viktorsta@users.noreply.github.com>
2021-03-09 00:05:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const abc = function() {
+    console.log("ova e funkcija abc vo script.js");
+}
+
+const abv = function() {
+    console.log("ova e funkcija abv vo server.js");
+}
+
+module.exports = abc;
---
+++ server.js
@@ -0,0 +1,5 @@
+console.log("zdravo");
+
+const abc = require("./script");
+
+abc();
